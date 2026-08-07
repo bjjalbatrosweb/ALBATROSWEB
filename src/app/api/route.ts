544ce992dc1d189074d1e997e11a93f5c70ffd60
Un.Mundo.Sin.Fronteras <joshyveco19@gmail.com>
2026-08-07 13:47:04 +0000

    versión prueba agostoto

--- src/app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import { GET as consultarCompras, POST as crearCompra } from '../route (1)';
-
-export const runtime = 'nodejs';
-export const dynamic = 'force-dynamic';
-
-export const GET = consultarCompras;
-export const POST = crearCompra;
